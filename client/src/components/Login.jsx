3947ae625cf8cf3aa6ff89b388f6ab11cf44a3fe
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-12-02 13:17:17 +0530

    Completed the frontend part

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,20 +1,27 @@
 import React, { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
+import { motion } from "framer-motion";
 
 const Login = () => {
   const [state, setState] = useState("Login");
-  const {setShowLogin}=useContext(AppContext);
+  const { setShowLogin } = useContext(AppContext);
 
-  useEffect(()=>{
-    document.body.style.overflow='hidden';
-    return()=>{
-        document.body.style.overflow='unset';
-    }
-  },[])
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, []);
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
-      <form className="relative bg-white p-10 rounded-xl text-slate-500">
+      <motion.form
+        initial={{ opacity: 0.2, y: 50 }}
+        transition={{ duration: 0.3 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        className="relative bg-white p-10 rounded-xl text-slate-500"
+      >
         <h1 className="text-center text-2xl text-neutral-700 font-medium">
           {state}
         </h1>
@@ -58,21 +65,31 @@ const Login = () => {
         {state === "Login" ? (
           <p className="mt-5 text-center">
             Dont't have an account?{" "}
-            <span onClick={()=>setState('Sign Up')} className="text-blue-600 cursor-pointer">Sign up</span>
+            <span
+              onClick={() => setState("Sign Up")}
+              className="text-blue-600 cursor-pointer"
+            >
+              Sign up
+            </span>
           </p>
         ) : (
           <p className="mt-5 text-center">
             Already have an account?{" "}
-            <span onClick={()=>setState('Login')} className="text-blue-600 cursor-pointer">Login</span>
+            <span
+              onClick={() => setState("Login")}
+              className="text-blue-600 cursor-pointer"
+            >
+              Login
+            </span>
           </p>
         )}
         <img
-          onClick={()=>setShowLogin(false)}
+          onClick={() => setShowLogin(false)}
           src={assets.cross_icon}
           alt=""
           className="absolute top-5 right-5 cursor-pointer"
         />
-      </form>
+      </motion.form>
     </div>
   );
 };
--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,29 +1,39 @@
 import React, { useState } from "react";
 import { assets } from "../assets/assets";
+import { motion } from "framer-motion";
 
 const Result = () => {
   const [image, setImage] = useState(assets.sample_img_1);
   const [isImageLoaded, setIsImageLoaded] = useState(false);
-  const [loading,setLoading] = useState(false);
-  const [input,setInput]=useState('');
+  const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState("");
 
-  const onSubmitHandler=async(e)=>{
-
-  }
+  const onSubmitHandler = async (e) => {};
 
   return (
-    <form onSubmit={onSubmitHandler} className="flex flex-col min-h-[90vh] justify-center items-center">
+    <motion.form
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      onSubmit={onSubmitHandler}
+      className="flex flex-col min-h-[90vh] justify-center items-center"
+    >
       <div>
         <div className="relative">
           <img src={image} className="max-w-sm rounded" alt="" />
-          <span className={`absolute bottom-0 left-0 h-1 bg-blue-500 ${loading?'w-full transition-all duration-[10s]':'w-0'}`} />
+          <span
+            className={`absolute bottom-0 left-0 h-1 bg-blue-500 ${
+              loading ? "w-full transition-all duration-[10s]" : "w-0"
+            }`}
+          />
         </div>
-        <p className={!loading?'hidden':''}>Loading.....</p>
+        <p className={!loading ? "hidden" : ""}>Loading.....</p>
       </div>
       {!isImageLoaded && (
         <div className="flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
           <input
-            onChange={e=>setInput(e.target.value)}
+            onChange={(e) => setInput(e.target.value)}
             value={input}
             type="text"
             placeholder="Describe what you want to generate"
@@ -38,19 +48,26 @@ const Result = () => {
         </div>
       )}
 
-      {isImageLoaded && <div className="flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full">
-        <p onClick={()=>{setIsImageLoaded(false)}} className="bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer">
-          Generate Another
-        </p>
-        <a
-          href={image}
-          download
-          className="bg-zinc-900 px-10 py-3 rounded-full cursor-pointer"
-        >
-          Download
-        </a>
-      </div>}
-    </form>
+      {isImageLoaded && (
+        <div className="flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full">
+          <p
+            onClick={() => {
+              setIsImageLoaded(false);
+            }}
+            className="bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer"
+          >
+            Generate Another
+          </p>
+          <a
+            href={image}
+            download
+            className="bg-zinc-900 px-10 py-3 rounded-full cursor-pointer"
+          >
+            Download
+          </a>
+        </div>
+      )}
+    </motion.form>
   );
 };
 
