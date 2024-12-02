c4e3669b0bf166402f0b5320b8a24fc96a03703d
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-12-02 11:58:43 +0530

    Home page fully animated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { motion } from "framer-motion";
+import {AppContext} from '../context/AppContext';
+import {useNavigate} from 'react-router-dom';
 
 const Header = () => {
+
+  const {user,setShowLogin} = useContext(AppContext);
+  const navigate=useNavigate()
+
+  const onClickHandler=()=>{
+    if(user){
+      navigate('/result')
+    }else{
+      setShowLogin(true);
+    }
+  }
   return (
     <motion.div
       className="flex flex-col justify-center items-center text-center my-20"
@@ -35,7 +48,7 @@ const Header = () => {
         Unleash your creativity with AI. Turn your imagination into visual art
         in seconds - just type, and watch the magic happen.
       </motion.p>
-      <motion.button className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full"
+      <motion.button onClick={onClickHandler} className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full"
          whileHover={{scale:1.05}}
          whileTap={{scale:0.95}}
          initial={{opacity:0}}
