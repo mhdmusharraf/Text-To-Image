cac7502fac8567a85f0183750799f42218e8f19d
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-12-07 00:14:26 +0530

    error resolved in auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -3,12 +3,12 @@ import jwt from "jsonwebtoken";
 const userAuth = async (req, res, next) => {
   const { token } = req.headers;
   if (!token) {
-    return res.json({ message: "Please login first" });
+    return res.json({success:false, message: "Please login first" });
   }
   try {
     const tokenDecode = await jwt.verify(token, process.env.JWT_SECRET);
     if (tokenDecode.id) {
-      req.body.userID = tokenDecode.id;
+      req.body.userId = tokenDecode.id;
     } else {
       return res.json({
         success: false,
