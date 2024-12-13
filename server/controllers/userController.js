adb72536b9c50c47bbb682f50eb23c4f98c58b8a
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2024-12-13 12:05:14 +0530

    working on razorpay

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import userModel from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import razorpay from "razorpay";
 
 const registerUser = async (req, res) => {
   try {
@@ -73,4 +74,48 @@ const userCredits = async (req, res) => {
   }
 };
 
-export { registerUser, loginUser,userCredits };
+const razorpayInstance = new razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+const paymentRazorpay = async (req, res) => {
+  try {
+    const { userId, planId } = req.body;
+    const user = await userModel.findById(userId);
+    if (!userid || !planId) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
+
+    let credits, plan, amount, date;
+
+    switch (planId) {
+      case "Basic":
+        plan = "Basic";
+        credits = 100;
+        amount = 10;
+        break;
+      case "Advanced":
+        plan = "Basic";
+        credits = 500;
+        amount = 50;
+        break;
+      case "Business":
+        plan = "Business";
+        credits = 5000;
+        amount = 250;
+        break;
+      default:
+        return res.json({ success: false, message: "Plan not found" });
+    }
+    date = Date.now();
+    const transactionData={
+      userId,plan,amount,credits,date
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { registerUser, loginUser, userCredits };
